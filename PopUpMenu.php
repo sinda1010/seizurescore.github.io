e158ecf7458405a85a8ad1102ed88915d11d85f2
sinda1010 <davidsinda79@gmail.com>
2014-09-14 19:44:41 -0400

    Create PopUpMenu.php

---
+++ PopUpMenu.php
@@ -0,0 +1,48 @@
+<HTML>
+<TITLE>Pull-Down Menus</TITLE>
+
+<HEAD>
+
+<STYLE TYPE="text/css">
+
+#menu1 { display : none }
+#menu2 { display : none }
+#menu3 { display : none }
+
+a:link {color:black; text-decoration:none}
+a:hover {color:blue; text-decoration:underline}
+
+</STYLE>
+
+<HEAD>
+
+
+<body style="background-color:lightblue">
+
+<h1 style="color:blue">Welcome to Seizure Score</h1>
+
+<p style="color:red;margin-left:20px">Monitoring your seizure progress</p>
+
+
+
+
+<TABLE BORDER="0" ALIGN="left">
+
+<TR>
+<TD VALIGN="top" WIDTH="200">
+<SPAN onMouseOver="document.all.menu1.style.display = 'block'" onMouseOut="document.all.menu1.style.display = 'block'">
+<FONT SIZE="-1"><b><IMG SRC="littlex.gif">&nbsp;Home</b></FONT></SPAN><BR>
+<SPAN ID="menu1" onClick="document.all.menu1.style.display = 'none'">
+
+<FONT SIZE="-1">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<IMG SRC="dailyhealth.jpeg">&nbsp;<A HREF="/tutors/frame1.html">Daily Log</A></FONT><BR>
+<FONT SIZE="-1">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<IMG SRC="littledash.gif">&nbsp;<A HREF="/tutors/tbl.html">Incident Log</A></FONT><BR>
+<FONT SIZE="-1">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<IMG SRC="littledash.gif">&nbsp;<A HREF="/beyond/dhtml.html">Incident Report</A></FONT><BR>
+<FONT SIZE="-1">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<IMG SRC="littledash.gif">&nbsp;<A HREF="/tutors/forms.html">Daily Report  </A></FONT>
+
+</SPAN>
+</TD>
+</TR>
+
+</TABLE>
+</BODY>
+</HTML>
