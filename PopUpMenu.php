73b1c8db993726f632d26853b7d46562d81e25df
sinda1010 <davidsinda79@gmail.com>
2014-09-15 13:02:42 -0400

    Update PopUpMenu.php

--- PopUpMenu.php
+++ PopUpMenu.php
@@ -14,12 +14,24 @@ a:hover {color:blue; text-decoration:underline}
 
 </STYLE>
 
+<script type="text/javascript">var switchTo5x=true;</script>
+<script type="text/javascript" src="http://w.sharethis.com/button/buttons.js"></script>
+<script type="text/javascript">stLight.options({publisher: "ur-215bfebf-d00b-6f93-45b5-bbb048802cd5", 
+doNotHash: false, doNotCopy: false, hashAddressBar: false});</script> //access the java script library for the share buttons 
+
 <HEAD>
 
 
 <body style="background-color:lightblue">
 
 <h1 style="color:blue">Welcome to Seizure Score</h1>
+
+<span class='st_sharethis_large' displayText='ShareThis'></span> //the share buttons for social media sites 
+<span class='st_facebook_large' displayText='Facebook'></span>
+<span class='st_twitter_large' displayText='Tweet'></span>
+<span class='st_linkedin_large' displayText='LinkedIn'></span>
+<span class='st_pinterest_large' displayText='Pinterest'></span>
+<span class='st_email_large' displayText='Email'></span>
 
 <p style="color:red;margin-left:20px">Monitoring your seizure progress</p>
 
